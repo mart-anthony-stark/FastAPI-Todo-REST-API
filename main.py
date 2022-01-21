e580192032cc8111feabb596c193ce6a9b22b5d7
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-21 20:09:29 +0800

    Added routes

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from urllib import response
+from database import (create_todo, fetch_all_todo,
+                      fetch_one_todo, update_todo, remove_todo)
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+
+from model import Todo
 
 # app object
 app = FastAPI()
+
 
 origins = [
     'https://localhost:3000'
@@ -16,26 +22,47 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get('/')
 def read_root():
     return {"Data": "Hello"}
 
+
 @app.get('/api/todo')
 async def get_todo():
-    return 1
+    response = await fetch_all_todo()
+    return response
 
-@app.get('/api/todo/{id}')
-async def get_todo_by_id(id: str):
-    return id
 
-@app.post('/api/todo')
-async def create_todo(todo):
-    return todo
+@app.get('/api/todo/{title}', response_model=Todo)
+async def get_todo_by_title(title: str):
+    response = await fetch_one_todo(title)
+    if(response):
+        return response
 
-@app.put('/api/todo/{id}')
-async def update_todo(id, data):
-    return id
+    raise HTTPException(404, f"There is no todo item with this title: {title}")
 
-@app.delete('/api/todo/{id}')
-async def delete_todo_by_id(id):
-    return id
+
+@app.post("/api/todo/", response_model=Todo)
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Something went wrong")
+
+
+@app.put("/api/todo/{title}", response_model=Todo)
+async def put_todo(title: str, desc: str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no todo with the title {title}")
+
+
+@app.delete('/api/todo/{title}')
+async def delete_todo_by_title(title):
+    response = await remove_todo(title)
+    if(response):
+        return "Successfully deleted todo"
+
+    raise HTTPException(400, "Something went wrong")
